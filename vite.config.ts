83a298993f59a00900a89bd5a9dca9c2ee221de0
NoOne451 <ranjithdurai111@gmail.com>
2024-01-01 22:49:51 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SOCIAL_MEDIA_APP/',
+  base: '/SocialMediaApp-FriendsFlow/',
 });
